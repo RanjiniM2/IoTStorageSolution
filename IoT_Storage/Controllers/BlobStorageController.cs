ee1f3974c08e9616da1d6ff51e2a628affb06d7a
RanjiniM2 <Ranjini.M2@mindtree.com>
2023-03-01 06:32:35 +0000

    Add project files.

---
+++ IoT_Storage/Controllers/BlobStorageController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Azure.Storage.Blobs.Models;
+using IoT_Storage.Repository;
+
+namespace IoT_Storage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlobStorageController : ControllerBase
+    {
+
+        [HttpPost("AddBob")]
+        public async Task<string>AddBlob(string blobName)
+        {
+            await BlobStorage.CreateBlob(blobName);
+            return null;
+        }
+
+        [HttpPut("UpdateBlobContent")]
+        public async Task UpdateBlobContent(string blobName,string file)
+        {
+            await BlobStorage.UpdateBlobContent(blobName,file);
+            
+        }
+
+        [HttpGet("GetBlobContent")]
+        public async Task<BlobProperties> GetBlobContent(string blobName, string file)
+        {
+           var data= await BlobStorage.GetBlobContent(blobName, file);
+            return data;
+        }
+
+        [HttpGet("GetBlob")]
+        public async Task<List<string>> GetBlob(string blobName, string file)
+        {
+            var data=await BlobStorage.GetBlob(blobName, file);
+            return data ;
+        }
+
+        [HttpGet("DownloadBlobContent")]
+        public async Task<BlobProperties> DownloadBlob(string blobName, string file)
+        {
+            var data=await BlobStorage.DownloadBlob(blobName, file);
+            return data;
+        }
+
+        [HttpDelete("DeleteBlobContent")]
+        public async Task<string> DeleteBlobContent(string blobName, string file)
+        {
+            await BlobStorage.DeleteBlobContent(blobName, file);
+            return null;
+           
+
+        }
+        public async Task<string> DeleteBlob(string blobName)
+        {
+            await BlobStorage.DeleteBlob(blobName);
+            return null;
+
+
+        }
+
+
+    }
+}
---
+++ IoT_Storage/Controllers/TableStorageController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using IoT_Storage.Repository;
+using IoT_Storage.Models;
+using System;
+using Azure.Data.Tables;
+
+namespace IoT_Storage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TableStorageController : ControllerBase
+    {
+        [HttpPost("AddTable")]
+        public async Task<string> AddTable(string tableName)
+        {
+            await TableStorage.AddTable(tableName);
+            return null;
+
+        }
+
+        [HttpPost("UpdateTable")]
+        public async Task<Details> UpdateTable(Details student,string tableName)
+        {
+            await TableStorage.UpdateTable(student,tableName);
+            return null;
+
+        }
+
+
+        [HttpGet("GetTableData")]
+        public async Task<Details> GetTableData(string tableName, string partitionKey, string rowKey)
+        {
+            var data=await TableStorage.GetTableData(tableName, partitionKey, rowKey);
+            return data;
+
+        }
+
+        [HttpGet("GetTable")]
+        public async Task<TableClient> GetTable(string tableName)
+        {
+            var data = await TableStorage.GetTable(tableName);
+            return data;
+
+        }
+
+
+        [HttpDelete("DeleteTableData")]
+        public static async Task DeleteTableData(string tableName, string partitionKey, string rowKey)
+        {
+            await TableStorage.DeleteTableData(tableName, partitionKey, rowKey);
+            return;
+
+        }
+
+        [HttpDelete("DeleteTable")]
+        public static async Task DeleteTable(string tableName)
+        {
+            await TableStorage.DeleteTable( tableName);
+            return;
+
+        }
+
+    }
+}
